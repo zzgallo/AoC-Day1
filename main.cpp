9a6aa27b3d22408edc2ca285a4454c5bbbf2bde1
zzgallo <mattgallo2002@gmail.com>
2024-12-11 13:35:47 -0500

    Added code to part two, of AoC day 1 challenge.

--- main.cpp
+++ main.cpp
@@ -26,6 +26,7 @@ Output total
 #include <algorithm>
 #include <vector>
 #include <numeric>
+#include <unordered_map>
 
 using namespace std;
 
@@ -2112,8 +2113,51 @@ for (int diff : absDifferences) {
 cout << endl;
 
 auto result = reduce(absDifferences.begin(), absDifferences.end());
-cout << "Total distance between all pairs is:  " << result;
+cout << "Total distance between all pairs is:  " << result << endl;
 
+// This begins part 2 of AoC day 1. The direction are as followed:
+/*
+This time, you'll need to figure out exactly how often each number from the left list appears in the right list. Calculate a total similarity score by adding up each number in the left list after multiplying it by the number of times that number appears in the right list.
+
+Here are the same example lists again:
+
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+
+For these example lists, here is the process of finding the similarity score:
+
+    The first number in the left list is 3. It appears in the right list three times, so the similarity score increases by 3 * 3 = 9.
+    The second number in the left list is 4. It appears in the right list once, so the similarity score increases by 4 * 1 = 4.
+    The third number in the left list is 2. It does not appear in the right list, so the similarity score does not increase (2 * 0 = 0).
+    The fourth number, 1, also does not appear in the right list.
+    The fifth number, 3, appears in the right list three times; the similarity score increases by 9.
+    The last number, 3, appears in the right list three times; the similarity score again increases by 9.
+
+So, for these example lists, the similarity score at the end of this process is 31 (9 + 4 + 0 + 0 + 9 + 9).
+*/
+
+// a "map" that will store the count of each value in list2
+unordered_map<int, int> list2Map;
+
+// Counts the occurenece of each value in list2
+for (int val : list2) {
+    list2Map[val]++;
+}
+
+// Track similarity score
+int similartityScore = 0;
+
+// iterate through the values in list1
+for (int val2 : list1) {
+    int count = list2Map[val2];
+
+    similartityScore += val2 * count;
+}
+cout << "Total similarity score is: " << similartityScore << endl;
 
 
 }
